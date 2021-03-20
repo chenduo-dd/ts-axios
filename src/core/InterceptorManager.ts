d5c392518e074b20f7fb93934b1c9a0f1864c26d
陈铎 <chenduo@MacBook-Pro.local>
2021-03-20 21:18:46 +0800

    first commit

---
+++ src/core/InterceptorManager.ts
@@ -0,0 +1,32 @@
+import { ResolvedFn, RejectedFn } from "../types"
+
+interface Interceptor<T> {
+    resolved: ResolvedFn<T>
+    rejected?: RejectedFn
+}
+
+export default class InterceptorManager<T> {
+    private interceptors: Array<Interceptor<T> | null>
+    constructor() {
+        this.interceptors = []
+    }
+    use(resolved: ResolvedFn<T>, rejected?: RejectedFn): number {
+        this.interceptors.push({
+            resolved,
+            rejected
+        })
+        return this.interceptors.length
+    }
+    eject(id: number): void {
+        if (this.interceptors[id]) {
+            this.interceptors[id] = null
+        }
+    }
+    forEach(fn: (intercepor: Interceptor<T>) => void): void {
+        this.interceptors.forEach(interceptor => {
+            if (interceptor !== null) {
+                fn(interceptor)
+            }
+        })
+    }
+}
